6031a306af3333742737075d5a627b2b3348538d
aanya6b <109451124+aanya6b@users.noreply.github.com>
2024-03-22 14:13:51 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 music1 = "";
 music2 = "";
 
+rightWristX = 0;
+rightWristY = 0;
+
+leftWristX = 0;
+leftWristY = 0;
+
+
 function preload()
 {
     music1 = loadSound("music.mp3");
@@ -14,6 +21,28 @@ function setup()
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+    console.log("Model is loaded");
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("Right wrist X = " + rightWristX + "Right wrist Y = " rightWristY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("Left wrist X = " + leftWristX + "Left wrist Y = " + leftWristY);
+    }
 }
 
 function draw()
